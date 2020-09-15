227835efcc4982340dbb6988c509b688eecb992a
Erika <erika@beasleys.com>
2020-09-14 22:09:23 -0400

    created controllers

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,17 +1,52 @@
-// Import ORM
-const orm = require('../config/orm.js');
+//Import express, and create express router
+const express = require('express');
+const router = express.Router();
 
-const burgers = {
-    all: (cb) => {
-        orm.all('burgers', (res) => cb(res));
-    },
-    create: (cols, vals, cb) => {
-        orm.create('burgers', cols, vals, (res) => cb(res));
-    },
-    update: (objColsVals, condition, cb) => {
-        orm.update('burgers', objColsVals, condition, (res) => cb(res));
-    }
-};
+//Import burgers from models
+const burger = require('../models/burgers.js');
 
-// Export DB functions
-module.exports = burgers;
+//Routes
+router.get('/', (req, res) => {
+    burger.selectAll((data) => {
+        //Make object housing burger data (from Database)
+        let hbsObj = {
+            burgers: data
+        };
+        //Create new view in index using handlebars object
+        res.render('index', hbsObj);
+    });
+});
+
+//Make new burger
+router.post('/api/burgers', (req, res) => {
+    burger.insertOne(
+        ['name', 'isDevoured'],
+        [req.body.name, req.body.isDevoured],
+        (result) => {
+            //Return ID of new burger
+            res.json({ id: result.insertId });
+        }
+    );
+});
+
+//Changing the isDevoured boolean
+router.put('/api/burgers/:id', (req, res) => {
+    let condition = `id = ${req.params.id}`;
+
+    burger.updateOne(
+        {
+            isDevoured: req.body.isDevoured
+        },
+        condition,
+        (result) => {
+            if (result.changedRows == 0) {
+                return res.status(404).end();
+            } else res.status(200).end();
+        }
+    );
+
+    res.end();
+});
+
+//Export router for server
+module.exports = router;
